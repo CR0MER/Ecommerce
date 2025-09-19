55cfb9ee7dc13626676ee613648406b54d08719a
CR0MER <cromergt@gmail.com>
2025-09-19 19:32:45 +0800

    reverted the revert

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar({ searchTerm, setSearchTerm }) {
             <img src={searchLogo} alt="Search" className="search-icon"/> 
             <input
             type="text"
-            placeholder="SEARCH PRODUCTS IN [INSERT NAME HERE] YEAH!!!!"
+            placeholder="SEARCH PRODUCTS IN Shupee🗿🗿🗿! YEAH!"
             className="nav-searchbox"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
